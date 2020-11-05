8d8df79028b6d0b2967b13c3dcf5b31ece330fda
Shibu TN <tnshibu@yahoo.com>
2020-11-06 03:21:00 +0530

    INITIAL

---
+++ 2_php.sh
@@ -0,0 +1,7 @@
+#--------------------------------------------------------------------------------------------------------------
+#install php
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt update
+sudo apt install php7.4 libapache2-mod-php7.4 php7.4-mbstring php7.4-mysql php-curl -y
+#--------------------------------------------------------------------------------------------------------------
